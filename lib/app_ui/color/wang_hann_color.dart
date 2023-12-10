a6c0f69115a00a3f8071bd71c2ce8f41c79b4030
Nancy <nancy62924@gmail.com>
2023-12-10 12:29:41 +0800

    style: add grey color

--- lib/app_ui/color/wang_hann_color.dart
+++ lib/app_ui/color/wang_hann_color.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 
 class WangHannColor {
   static const white = Colors.white;
+  static final grey = Colors.white.withOpacity(0.5);
   static const black = Color(0xFF202020);
 }
